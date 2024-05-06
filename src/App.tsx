9772e5be9e0adf26bc0fe10cbc4b5b44003210a7
Marius A. Rabenarivo <mariusrabenarivo@gmail.com>
2024-05-06 17:21:07 +0400

    Initial commit :tada:
    Migration to Vite and use @react-google-maps/api

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import './App.css'
+import React from "react";
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: -18.525,
+  lng: 47.9
+};
+
+function App() {
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+  
+  return isLoaded ? (
+
+    <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+      <></>
+      </GoogleMap>
+  ) : <></>
+}
+
+export default App
