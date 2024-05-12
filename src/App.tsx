62bdb50e0ebb077e012177a8e4e2fac55c915865
Marius A. Rabenarivo <mariusrabenarivo@gmail.com>
2024-05-12 20:59:17 +0400

    Switch map library, add map features - Use @vis.gl/react-google-maps instead of @react-google-maps/api (issue with refreshing Polyline) - Display lines with associated ways and stops on the map

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,43 @@
-import { useRef } from "react";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import {APIProvider, Map, Marker} from '@vis.gl/react-google-maps';
+import {Polyline} from './components/polyline.tsx';
 import { Stops, Lines } from "./components";
-// import { useMap } from "./contexts/map";
+import { useMap } from "./contexts/map";
 
 const containerStyle = {
-  width: "400px",
-  height: "400px",
+  width: "640px",
+  height: "480px",
 };
 
 const center = {
-  lat: -18.525,
-  lng: 47.9,
+  lat: -18.9127,
+  lng: 47.49855
 };
 
 const App = () => {
-  const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
-    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
-  });
 
-  // const { stops, ways } = useMap();
-
-  const mapRef = useRef<google.maps.Map | null>();
-
-  const onLoad = (map: google.maps.Map) => {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
-    mapRef.current = map;
-  };
-
-  const onUnmount = () => {
-    mapRef.current = null;
-  };
-
+  const {stops, ways} = useMap();
+  
   return (
-    isLoaded && (
       <>
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={center}
-          zoom={10}
-          onLoad={onLoad}
-          onUnmount={onUnmount}
-        >
-          {/* TODO: markers, polylines, etc */}
-        </GoogleMap>
-        <div className="grid-container">
-          <div>
-            <Stops />
-          </div>
-          <div>
-            <Lines />
-          </div>
-        </div>
+      <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY as string}>
+      <Map
+    style={containerStyle}
+    defaultCenter={center}
+    defaultZoom={10}
+      >
+      { stops.map((stop, index) => <Marker key={index} position={stop.location} title={stop.name} />) }
+    { ways.map((path, index) => <Polyline key={index} path={path} />) }
+    </Map>
+      </APIProvider>
+      <div className="grid-container">
+      <div>
+      <Stops />
+      </div>
+      <div>
+      <Lines />
+      </div>
+      </div>
       </>
-    )
   );
 };
 
