1ed5e44ad399b3b776849a36359ab5f5af10010b
Marius A. Rabenarivo <mariusrabenarivo@gmail.com>
2024-05-28 22:08:32 +0400

    Run Cypress in Github Actions

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -6,26 +6,23 @@ describe("testing test", () => {
 
 describe("can display the app", () => {
   beforeEach(() => {
-    cy.intercept("GET", "/api/lines", {
+    cy.intercept("GET", "**/api/lines", {
       fixture: "lines.json",
     }).as("linesApi");
 
-    cy.intercept("GET", "/api/stops/Ligne_D_Horizon_Vasacoss_R", {
+    cy.intercept("GET", "**/api/stops/Ligne_D_Horizon_Vasacoss_R", {
       fixture: "stops.json",
     }).as("stopsApi");
 
-    cy.intercept("GET", "/api/ways/Ligne_D_Horizon_Vasacoss_R", {
+    cy.intercept("GET", "**/api/ways/Ligne_D_Horizon_Vasacoss_R", {
       fixture: "ways.json",
     }).as("waysApi");
 
-    cy.intercept("GET", "/api/busStops", {
+    cy.intercept("GET", "**/api/busStops", {
       fixture: "busStops.json",
     }).as("busStopsApi");
 
     cy.visit("http://localhost:5173/");
-
-    cy.wait("@linesApi");
-    cy.wait("@busStopsApi");
   });
 
   it("Title of the page contains BetaX", () => {
@@ -33,15 +30,17 @@ describe("can display the app", () => {
   });
 
   it("The bus line list is displayed", () => {
-    cy.get("#root > div:nth-child(2) > div:nth-child(2) > ul", {
-      timeout: 1000,
-    })
+    cy.wait("@linesApi")
+      .get("#root > div:nth-child(2) > div:nth-child(2) > ul", {
+        timeout: 1000,
+      })
       .children(0)
       .should("contain", "Ligne_D_Horizon_Vasacoss_R");
   });
 
   it("The bus stop list is displayed", () => {
-    cy.get("#root > div:nth-child(2) > div:nth-child(1) > ul")
+    cy.wait("@busStopsApi")
+      .get("#root > div:nth-child(2) > div:nth-child(1) > ul")
       .children(0)
       .should("contain", "Toto-bato Ambodifilao");
   });
@@ -50,6 +49,7 @@ describe("can display the app", () => {
     cy.get("#root > div:nth-child(2) > div:nth-child(2) > ul")
       .children(0)
       .click()
+      .wait("@stopsApi")
       .get("#root > div:nth-child(1) > div:nth-child(2) > div > h2")
       .should("contain", "Ligne_D_Horizon_Vasacoss_R")
       .get("#root > div:nth-child(1) > div:nth-child(2) > div > ul")
